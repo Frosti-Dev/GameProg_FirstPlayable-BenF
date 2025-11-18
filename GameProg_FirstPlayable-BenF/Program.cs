7570f0ea120c5c639cae013626291141bd57e147
Frosti-Dev <bennyfranken3@gmail.com>
2025-11-18 15:26:05 -0400

    Initial commit

---
+++ GameProg_FirstPlayable-BenF/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProg_FirstPlayable_BenF
+{
+    internal class Program
+    {
+        static int playerHealth;
+        static int playerPosX = 5;
+        static int playerPosY = 5;
+        static int scale = 1;
+
+        static char[,] map = new char[,] // dimensions defined by following data:
+    {
+        {'^','^','^','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`'},
+        {'^','^','`','`','`','`','*','*','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','~','~','~','`','`','`'},
+        {'^','^','`','`','`','*','*','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','~','~','~','`','`','`','`','`'},
+        {'^','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`'},
+        {'`','`','`','`','~','~','~','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`'},
+        {'`','`','`','`','~','~','~','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`'},
+        {'`','`','`','~','~','~','~','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','^','^','`','`','`','`','`','`'},
+        {'`','`','`','`','`','~','~','~','`','`','`','`','`','`','`','`','`','`','`','`','`','^','^','^','^','`','`','`','`','`'},
+        {'`','`','`','`','`','~','~','~','~','`','`','`','`','`','`','`','`','`','`','`','`','`','`','^','^','^','^','`','`','`'},
+        {'`','`','`','`','`','`','`','~','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`'},
+        {'`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`'},
+        {'`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`'},
+    };
+        static void DisplayMap(int scale)
+        {
+           
+            //border top
+            Console.Write("+");
+            for (int k = 0; k < map.GetLength(0) * scale * 2; k++)
+            {
+                Console.Write('-');
+            }
+            Console.Write('+');
+            Console.WriteLine(" ");
+
+            //print map
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+
+                int row = 0;
+                while (row < scale)
+                {
+                    Console.Write('|');
+                    for (int j = 0; j < map.GetLength(0); j++)
+                    {
+                        int timer = 0;
+                        while (timer < scale)
+                        {
+                            Console.Write(map[i, j] + " ");
+                            timer++;
+                        }
+                    }
+                    Console.Write('|');
+                    Console.WriteLine(" ");
+                    row++;
+                }
+            }
+
+            //bottom border
+            Console.Write("+");
+            for (int f = 0; f < map.GetLength(0) * scale * 2; f++)
+            {
+                Console.Write('-');
+            }
+            Console.Write('+');
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Map legend: ");
+            Console.WriteLine("^ = mountain");
+            Console.WriteLine("` = grass");
+            Console.WriteLine("~ = water");
+            Console.WriteLine("* = trees");
+        }
+
+        static void PlayerDraw(int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write("X");
+            Console.SetCursorPosition(x, y);
+        }
+
+        static void PlayerUpdate()
+        {
+
+            ConsoleKeyInfo keyinfo = Console.ReadKey(true);
+
+            switch (keyinfo.Key)
+            {
+                case ConsoleKey.W:
+                    playerPosY -= 1;
+                    if(playerPosY <= 0)
+                    {
+                        playerPosY += 1;
+                    }
+                    break;
+
+                case ConsoleKey.A:
+
+                    playerPosX -= 1;
+
+                    if(playerPosX <= 0)
+                    {
+                        playerPosX += 1;
+                    }
+
+                    break;
+
+                case ConsoleKey.S:
+
+                    playerPosY += 1;
+
+                    if(playerPosY > 12 * scale)
+                    {
+                        playerPosY -= 1;
+                    }
+                    break;
+
+                case ConsoleKey.D:
+
+                    playerPosX += 1;
+
+                    if(playerPosX > 24 * scale)
+                    {
+                        playerPosX -= 1;
+                    }
+                    break;
+
+                case ConsoleKey.Escape:
+
+                    Console.Clear();
+                    Console.WriteLine("You Quit.");
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            
+            while (true)
+            {
+                Console.Clear();
+                DisplayMap(scale);
+                PlayerDraw(playerPosX, playerPosY);
+                PlayerUpdate();
+
+                Debug.Print($"Cords: {playerPosX},{playerPosY}");
+            }
+        }
+    }
+}
